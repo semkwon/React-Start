32360315daa595f0a6378bb1f42e5c61d88df139
권세명 <3pkwon@gmail.com>
2023-03-30 15:43:54 +0900

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import newBalance from "./images/newBalance.jpg";
 function App() {
   return (
     <div className="bg-red-100 min-h-screen">
       <header className="bg-blue-100 h-12 md:h-20">
-        <div className="max-w-screen-xl mx-auto h-full flex justify-between place-items-center">
-          <div>권세명's Portfolio</div>
-          <ul className="flex gap-2 md:gap-8 text-xs md:text-base">
-            <li>Introduce</li>
-            <li>Portfolio</li>
-            <li>Contact Me</li>
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+          {/* <div>h662's Portfolio</div> */}
+          <div>
+            <img className="w-12" src={newBalance} alt="newBalance" />
+          </div>
+          <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
+            <li className="bg-indigo-300 btn-style">Introduce</li>
+            <li className="bg-purple-300 btn-style">Portfolio</li>
+            <li className="bg-blue-300 btn-style">Contact Me</li>
           </ul>
         </div>
       </header>
