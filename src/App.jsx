b3b2cbba02e28af474fce174cece51a2aa7a8333
권세명 <3pkwon@gmail.com>
2023-03-30 15:08:09 +0900

    react day1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-100 h-12 md:h-20">
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between place-items-center">
+          <div>권세명's Portfolio</div>
+          <ul className="flex gap-2 md:gap-8 text-xs md:text-base">
+            <li>Introduce</li>
+            <li>Portfolio</li>
+            <li>Contact Me</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
